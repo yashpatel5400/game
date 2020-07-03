47800adc1ff878e6effaaed53019b409b832d9df
yashpatel5400 <ypatel5400@gmail.com>
2020-07-03 10:01:40 -0700

    Add movable camera

--- sdf.fs
+++ sdf.fs
@@ -29,6 +29,21 @@ struct Light
     vec3 color;
 };
 
+struct Ray {
+    vec3 point;
+    vec3 direction; // assumed normalized
+};
+
+struct Camera {
+    vec3 position;
+
+    // camera coordinate system
+    // note that any one can be determined from the other two as norm(cross prod)
+    vec3 up;
+    vec3 right;
+    vec3 forward;
+};
+
 bool insideSphere(Ball ball, vec3 pos) {
     if (length(ball.position - pos) < ball.radius) {
         return true;
@@ -36,25 +51,38 @@ bool insideSphere(Ball ball, vec3 pos) {
     return false;
 }
 
+vec3 cameraToWorld(Camera camera, vec3 coord) {
+    mat3 transform = mat3(camera.right, camera.up, -camera.forward);
+    return camera.position + transform * coord;
+}
+
 void main()
 {
     // assume right handed coord system, so X/Y are left/right and z into the screen
-    Light light = Light(0.50, vec3(1.0, 1.0, -2.5), vec3(1.0)); // white light
-    Ball ball = Ball(1.0, vec3(0.0, 0.0, -2.5), vec3(1.0, 0.0, 0.0));
+    Light light = Light(0.50, vec3(1.0, 1.0, 0.0), vec3(1.0)); // white light
+    Ball ball = Ball(1.0, vec3(0.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0));
 
     float bounceFreq = 2.0;
     ball.position.y = abs(sin(bounceFreq * time));
 
     // we assume the rays are being cast from the origin, so ray point is dir * t
-    vec3 pos = vec3(screenPos, -1.0);
-    vec3 dir = normalize(pos);
-    const float kStepSize = 0.05;
-    const int kNumSteps = 20;
-
+    vec3 origin = vec3(0.0);
+    vec3 cameraPos = vec3(0.0, 0.0, 5.0);
+    vec3 cameraUp = vec3(0.0, 1.0, 0.0); // assume camera on x-z plane
+    vec3 cameraForward = normalize(origin - cameraPos); // camera faces origin
+    vec3 cameraRight = normalize(cross(cameraForward, cameraUp));
+    Camera camera = Camera(cameraPos, cameraUp, cameraRight, cameraForward);
+    
+    vec3 dir = normalize(vec3(screenPos, -1.0));
+    vec3 cameraRay = origin; // we track this in camera space and then transform
+    
+    const float kStepSize = 1.0;
+    const int kNumSteps = 50;
     for (int step = 0; step < kNumSteps; step++) {
-        pos += dir * kStepSize;
-        if (insideSphere(ball, pos)) {
-            float lightStrength = light.intensity / length(pos - light.position);
+        cameraRay += dir * kStepSize;
+        vec3 worldRay = cameraToWorld(camera, cameraRay);
+        if (insideSphere(ball, worldRay)) {
+            float lightStrength = light.intensity / length(worldRay - light.position);
             FragColor = vec4(ball.color * lightStrength, 1.0);
             return;  
 		}
