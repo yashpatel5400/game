c144b4cc152530764a0ca8c6886f47e29eff161a
yashpatel5400 <ypatel5400@gmail.com>
2020-07-03 11:05:18 -0700

    basic scene -- this is so cool!

--- sdf.fs
+++ sdf.fs
@@ -60,14 +60,14 @@ void main()
 {
     // assume right handed coord system, so X/Y are left/right and z into the screen
     Light light = Light(0.50, vec3(1.0, 1.0, 0.0), vec3(1.0)); // white light
-    Ball ball = Ball(1.0, vec3(0.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0));
+    Ball ball = Ball(1.0, vec3(0.0, 1.0, 0.0), vec3(1.0, 0.0, 0.0));
 
     float bounceFreq = 2.0;
-    ball.position.y = abs(sin(bounceFreq * time));
+    ball.position.y += abs(sin(bounceFreq * time));
 
     // we assume the rays are being cast from the origin, so ray point is dir * t
     vec3 origin = vec3(0.0);
-    vec3 cameraPos = vec3(0.0, 0.0, 5.0);
+    vec3 cameraPos = vec3(5.0 * sin(time), 1.0, 5.0 * cos(time));
     vec3 cameraUp = vec3(0.0, 1.0, 0.0); // assume camera on x-z plane
     vec3 cameraForward = normalize(origin - cameraPos); // camera faces origin
     vec3 cameraRight = normalize(cross(cameraForward, cameraUp));
@@ -76,17 +76,32 @@ void main()
     vec3 dir = normalize(vec3(screenPos, -1.0));
     vec3 cameraRay = origin; // we track this in camera space and then transform
     
-    const float kStepSize = 1.0;
-    const int kNumSteps = 50;
+    const float kStepSize = .10;
+    const int kNumSteps = 100;
     for (int step = 0; step < kNumSteps; step++) {
         cameraRay += dir * kStepSize;
         vec3 worldRay = cameraToWorld(camera, cameraRay);
+        
+        // show the light source
+        if (length(light.position - worldRay) < 0.10) {
+            FragColor = vec4(1.0);
+            return;  
+		}
+
+        // character
         if (insideSphere(ball, worldRay)) {
             float lightStrength = light.intensity / length(worldRay - light.position);
             FragColor = vec4(ball.color * lightStrength, 1.0);
             return;  
 		}
+
+        // floor
+        if (worldRay.y < 0.0) {
+            float lightStrength = light.intensity / length(worldRay - light.position);
+            FragColor = vec4(vec3(0.0, 1.0, 0.0) * lightStrength, 1.0);
+            return;  
+		}
 	}
 
-    FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    FragColor = vec4(0.0, 0.0, 1.0, 1.0);
 }
